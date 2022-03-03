4c8685d857a5a0ce7e60399e4c7ec20b8feb0770
brenoos <breno.oliver07@gmail.com>
2022-03-03 15:12:19 -0300

    feat: filtering films

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Form,
   LinksFunction,
   LoaderFunction,
   MetaFunction,
@@ -6,10 +7,6 @@ import {
 } from 'remix';
 import { Film } from '~/services/Film/Film';
 import { getFilms } from '~/services/Film/getFilms';
-
-export const loader: LoaderFunction = async () => {
-  return getFilms();
-};
 
 export const meta: MetaFunction = () => ({
   title: 'Films | Studio Ghibli',
@@ -20,13 +17,19 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: 'styles' },
 ];
 
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const title = url.searchParams.get('title');
+  return getFilms(title);
+};
+
 export default function FilmsIndex() {
   const films = useLoaderData<Film[]>();
   return (
     <div className="p-16 font-sans">
       <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
 
-      <form className="py-5">
+      <Form reloadDocument className="py-5">
         <label className="font-bold">
           Search{' '}
           <input
@@ -42,7 +45,7 @@ export default function FilmsIndex() {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2">
           Search
         </button>
-      </form>
+      </Form>
 
       <div className="grid grid-cols-4 gap-4">
         {films.map(film => (
--- app/services/Film/getFilms.ts
+++ app/services/Film/getFilms.ts
@@ -1,7 +1,11 @@
 import { Film } from './Film';
 
-export async function getFilms(): Promise<Film[]> {
+export async function getFilms(title?: string | null): Promise<Film[]> {
   const response = await fetch('https://ghibliapi.herokuapp.com/films');
 
-  return response.json();
+  const films: Film[] = await response.json();
+
+  return films.filter(film =>
+    title ? film.title.toLowerCase().includes(title.toLowerCase()) : true,
+  );
 }
