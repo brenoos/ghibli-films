63befbf097e6c2349c4015ea9a3b6e260ebfedba
brenoos <breno.oliver07@gmail.com>
2022-03-16 10:54:22 -0300

    feat: error boundaries

--- app/routes/films/$filmId/characters.$characterId.tsx
+++ app/routes/films/$filmId/characters.$characterId.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, useLoaderData } from 'remix';
+import { LoaderFunction, useCatch, useLoaderData } from 'remix';
 import invariant from 'tiny-invariant';
 import { FilmCharacter } from '~/services/Film';
 import { getFilmCharacter } from '~/services/Film/getFilmCharacter';
@@ -23,6 +23,42 @@ export default function Character() {
           <li>Eye Color: {eye_color}</li>
           <li>Hair Color: {hair_color}</li>
         </ul>
+      </div>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="mb-3">
+        <div className="text-3xl mb-2">Details</div>
+        <div className="p-4 rounded shadow-lg border bg-orange-200 border-orange-600">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            {caught.statusText}
+          </div>
+          <p>
+            {caught.status} {caught.statusText}
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  throw new Error('Unkown Error');
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div className="mb-3">
+      <div className="text-3xl mb-2">Details</div>
+      <div className="p-4 rounded shadow-lg border bg-rose-200 border-rose-600">
+        <div className="text-gray-700 font-bold text-xl mb-2">
+          Uh oh... Sorry something went wrong!
+        </div>
+        <p>{error?.message}</p>
       </div>
     </div>
   );
